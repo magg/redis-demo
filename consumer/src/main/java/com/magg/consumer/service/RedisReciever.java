705eb143dd937be7430be246fb257091120b9bcc
Miguel Gonzalez <miguel@okaythis.com>
2021-09-24 12:05:17 -0500

    id for each consumer app

--- consumer/src/main/java/com/magg/consumer/service/RedisReciever.java
+++ consumer/src/main/java/com/magg/consumer/service/RedisReciever.java
@@ -3,6 +3,7 @@ package com.magg.consumer.service;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,6 +13,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class RedisReciever
 {
+    @Value("${spring.application.name}")
+    private String appName;
+
+    private String queueName;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisReciever.class);
 
@@ -30,16 +35,18 @@ public class RedisReciever
 
     @Scheduled(fixedRate = 5000)
     public void listen(){
+
+        queueName = "statements.tmp.queue." + appName;
         try {
             //Take out the message and put it in the temporary queue
 
-            String data = reactiveListOps.rightPopAndLeftPush(LIST_NAME, "tmp-queue");
+            String data = reactiveListOps.rightPopAndLeftPush(LIST_NAME, queueName);
 
             Thread.sleep(1000);
             //reactiveListOps.rightPop("tmp-queue");//Non-blocking
 
             //Blocking brpop, block when there is no data in the List, the parameter 0 means that the block will continue until data appears in the List
-            String str = reactiveListOps.rightPop("tmp-queue"); //block, remove the temporary Queue
+            String str = reactiveListOps.rightPop(queueName); //block, remove the temporary Queue
 
             log.info("Data - " + str + " received through Redis List - ");
 
